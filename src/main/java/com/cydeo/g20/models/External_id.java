5f4fb230795111e753e3215950f3a28e24b0a725
Dmytro Kulikovskykh <straum.phone@gmail.com>
2023-05-17 02:12:21 +0300

    - put correct Json annotation in the class External_id.

--- src/main/java/com/cydeo/g20/models/External_id.java
+++ src/main/java/com/cydeo/g20/models/External_id.java
@@ -1,10 +1,10 @@
 package com.cydeo.g20.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 @Data
 public class External_id {
-    @JsonProperty("isrc")
+    @JsonAlias({"isrc", "upc"})
     private String upc;
 }
